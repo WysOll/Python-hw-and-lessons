d1b6b9562c875925eecab27e7516651d2cae80be
Oleksii Kosenko <73740510+WysOll@users.noreply.github.com>
2021-12-27 19:46:51 +0200

    Delete lesson20 directory

--- lesson20/les20-11.py
+++
@@ -1,6 +0,0 @@
-dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-for k, v in dic.items():
-    if v%2==0:
-        print(f"{k} is even")
-    else:
-        print(f"{k} is odd")
--- lesson20/les20-8.py
+++
@@ -1,4 +0,0 @@
-a={'a': 2, 'b': 4, 'c': 6, 'd': 8}
-for k, v in a.items():
-    if v>2:
-        print(f"{k}:{v}")
